5258019848cf1b775de8d1eeaf57773542b2ec88
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2020-03-21 11:38:43 +0300

    FPSCounter one second waiting fix

--- osu/desktop/interaction/FPSCounter.cpp
+++ osu/desktop/interaction/FPSCounter.cpp
@@ -10,8 +10,7 @@ namespace osu {
         _range = range;
         now = 0;
         durations = new double[range];
-        past = high_resolution_clock::now();
-        future = high_resolution_clock::now();
+        future = past = high_resolution_clock::now();
     }
 
     FPSCounter::~FPSCounter() {
@@ -27,28 +26,19 @@ namespace osu {
         _range = range;
         now = 0;
         durations = new double[range];
-//            perPeriod = 0;
         past = high_resolution_clock::now();
         future = high_resolution_clock::now();
         delete[] old;
     }
 
     void FPSCounter::countFPSAndSleep() {
-        duration<double, std::milli> length =
-                (future = high_resolution_clock::now()) - past;
+        auto length = (future = high_resolution_clock::now()) - past;
         if (++now == _range) {
             now = 0;
         }
         durations[now] = length.count();
         past = future;
-//            struct tm *timer_info;
-//            char buff[26];
-//            time_t a = system_clock::to_time_t(system_clock::now());
-//            timer_info = localtime(&a);
-//            strftime(buff, 26, "%Y-%m-%d %H:%M:%S", timer_info);
-//            std::cout << buff << " " << getFPS() << " " << durations[now] << " " << now << std::endl;
-        std::this_thread::sleep_until(
-                time_point_cast<seconds>(future) + microseconds(1000000 * (now + 1) / _range));
+        std::this_thread::sleep_until(future + microseconds(1000000 / _range * 2) - length);
     }
 
     float FPSCounter::getDelta() {
