89ae22b768c6af6e60f1b355191c5d3feb082441
MasterLogick <31408232+MasterLogick@users.noreply.github.com>
2020-06-29 19:16:47 +0300

    StringUtills static modifier removed

--- osu/desktop/logic/utill/StringUtills.h
+++ osu/desktop/logic/utill/StringUtills.h
@@ -11,46 +11,46 @@
 #include <boost/algorithm/string.hpp>
 
 namespace osu {
-    static inline void ltrim(std::string &s) {
+    inline void ltrim(std::string &s) {
         s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
             return !std::isspace(ch);
         }));
     }
 
-    static inline void rtrim(std::string &s) {
+    inline void rtrim(std::string &s) {
         s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
             return !std::isspace(ch);
         }).base(), s.end());
     }
 
-    static inline void trim(std::string &s) {
+    inline void trim(std::string &s) {
         ltrim(s);
         rtrim(s);
     }
 
-    static inline std::string ltrim_copy(std::string &s) {
+    inline std::string ltrim_copy(std::string &s) {
         ltrim(s);
         return s;
     }
 
-    static inline std::string rtrim_copy(std::string &s) {
+    inline std::string rtrim_copy(std::string &s) {
         rtrim(s);
         return s;
     }
 
-    static inline std::string trim_copy(std::string &s) {
+    inline std::string trim_copy(std::string &s) {
         trim(s);
         return s;
     }
 
-    static std::pair<std::string, std::string> splitKeyValPair(std::string &kpVar, char separator = ':') {
+    std::pair<std::string, std::string> splitKeyValPair(std::string &kpVar, char separator = ':') {
         size_t pos = kpVar.find(separator);
         std::string val{kpVar.substr(pos + 1)};
         ltrim(val);
         return std::pair<std::string, std::string>(kpVar.substr(0, pos), val);
     }
 
-    static std::pair<std::string, std::string> splitKeyValPair(std::string &kpVar, int skip, char separator = ':') {
+    std::pair<std::string, std::string> splitKeyValPair(std::string &kpVar, int skip, char separator = ':') {
         size_t pos = -1;
         for (int i = 0; i < skip; ++i) {
             pos = kpVar.find(separator, pos + 1);
@@ -61,7 +61,7 @@ namespace osu {
         return std::pair<std::string, std::string>(kpVar.substr(0, pos), val);
     }
 
-    static std::vector<std::string> split(std::string &str, std::string separator) {
+    std::vector<std::string> split(std::string &str, std::string separator) {
         std::vector<std::string> result;
         boost::split(result, str, boost::is_any_of(separator));
         return result;
